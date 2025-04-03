499e32753a1fc2f349937cc264055bec032766f2
thewisepup <wisepup257@gmail.com>
2025-04-02 17:58:40 -0700

    refactor dot cluster generation code

--- processing/color_transformer.py
+++ processing/color_transformer.py
@@ -12,15 +12,23 @@ class ColorTransformer:
         if self.config.debug_mode:
             print("--Transforming image to dot clusters--")
 
-        dot_clusters = []
-        height, width, _ = img.shape
+        # Get all pixels at once and reshape to 2D array of pixels
+        pixels = img.reshape(-1, 3)
 
-        for y in range(height):
-            for x in range(width):
-                pixel = img[y, x]
-                selected_color = self._select_dot_cluster_colors(pixel, color_palette)
-                dot_cluster = DotCluster((x, y), pixel, selected_color)
-                dot_clusters.append(dot_cluster)
+        # Get coordinates for all pixels
+        y_coords, x_coords = np.unravel_index(np.arange(pixels.shape[0]), img.shape[:2])
+        coordinates = np.column_stack((x_coords, y_coords))
+
+        # Vectorized color selection for all pixels at once
+        selected_colors = np.array(
+            [self._select_dot_cluster_colors(pixel, color_palette) for pixel in pixels]
+        )
+
+        # Create dot clusters using list comprehension
+        dot_clusters = [
+            DotCluster((x, y), pixel, color)
+            for (x, y), pixel, color in zip(coordinates, pixels, selected_colors)
+        ]
         print(len(dot_clusters))
 
     def _select_dot_cluster_colors(self, pixel: np.ndarray, color_palette: np.ndarray):
