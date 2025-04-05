23e92b984771219a9ac4af4fe09f7f6b2fa586af
thewisepup <wisepup257@gmail.com>
2025-04-05 11:05:30 -0700

    update compute dot weights method to do matrix multiplication

--- models/dot_cluster.py
+++ models/dot_cluster.py
@@ -3,7 +3,6 @@ import numpy as np
 
 class DotCluster:
 
-    # TODO: pass in configs that will be used to generate weights and counts
     def __init__(self, position, pixel_color, selected_colors, alpha, I):
         self.position = position
         self.pixel_color = pixel_color
@@ -18,20 +17,24 @@ class DotCluster:
 
     def _compute_color_weights(self) -> np.ndarray:
         """
-        Compute the weights for each selected_color to use for the pixel dot cluster
-
-        Args:
-            pixel_rgb: rgb value of input pixel to generate dot cluster for
-            selected_colors: list of (3) colors to generate dot cluster
+        Compute the weights (Q) for each selected color to use for the pixel dot cluster.
+        Solves the equation Q = C^-1 · P where:
+        - C is a 3x3 matrix containing the RGB values of the three selected colors
+        - P is a 3x1 matrix containing the RGB values of the original pixel
+        - Q is a 3x1 matrix of weights for each selected color
 
         Returns:
-            weights: np.ndarray shape(3,)
+            np.ndarray: A flattened array of weights for each selected color
         """
         # TODO: convert HSV to RGB, lhs is using HSV values in equation
         # TODO: double check math
-        lhs = np.linalg.pinv(np.array(self.selected_colors))
-        rhs = self.pixel_color.T
-        return lhs * rhs
+
+        C = np.array(self.selected_colors)
+        P = np.array(self.pixel_color).reshape(3, 1)
+        C_inv = np.linalg.pinv(C)
+        Q = C_inv @ P  # Matrix multiplication
+
+        return Q.flatten()  # Return as a flat array
 
     def _compute_color_dot_counts(self):
         """
